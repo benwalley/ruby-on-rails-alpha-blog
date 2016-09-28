f8aff5c30c1032896e97c845ac1255188aba8254
Ben Walley <benwalleyorigami@gmail.com>
2016-09-28 16:21:00 +0000

    create new article controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximun: 50}
-  validates :description, presence true, length: { minimum: 10, maximum: 2000}
-  
+  validates :description, presence: true, length: { minimum: 10, maximum: 2000}
 end
